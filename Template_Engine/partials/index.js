13dfc99e43a2be9f819c2df8c5d22a6012c84140
Rafael Luiz Batistoti <97912202+RafaelBatistoti@users.noreply.github.com>
2022-05-07 08:32:16 -0300

    Nesse modulo aprendemos mais sobre Handlebars

---
+++ Template_Engine/partials/index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+const hbs = exphbs.create({
+  partialsDir: ["views/patials"],
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.get("/blog", (req, res) => {
+  const posts = [
+    {
+      title: "Blog",
+      category: "php",
+      body: "Linguagem de programação",
+      comments: 4,
+    },
+    {
+      title: "Blog",
+      category: "node",
+      body: "Linguagem de programação",
+      comments: 3,
+    },
+    {
+      title: "Blog",
+      category: "html",
+      body: "Linguagem de programação",
+      comments: 2,
+    },
+  ];
+
+  res.render("blog", { posts });
+});
+
+app.get("/post", (req, res) => {
+  const post = {
+    title: "Blog",
+    category: "Java",
+    body: "Linguagem de programação",
+    comments: 4,
+  };
+
+  res.render("blogPost", { post });
+});
+
+app.get("/", (req, res) => {
+  const user = {
+    name: "Rafael",
+    lastname: "Batistoti",
+  };
+
+  const auth = true;
+  const approved = false;
+
+  res.render("home", { user: user, auth, approved });
+});
+
+app.get("/dashboard", (req, res) => {
+  const items = ["a", "b", "c", "d", "e"];
+
+  res.render("dashboard", { items });
+});
+
+app.listen(3000, () => console.log("Running"));
---
+++ Template_Engine/with/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.get("/post", (req, res) => {
+  const post = {
+    title: "Blog",
+    category: "Java",
+    body: "Linguagem de programação",
+    comments: 4,
+  };
+
+   res.render('blogPost', {post})
+
+
+});
+
+app.get("/", (req, res) => {
+  const user = {
+    name: "Rafael",
+    lastname: "Batistoti",
+  };
+
+  const auth = true;
+  const approved = false;
+
+  res.render("home", { user: user, auth, approved });
+});
+
+app.get("/dashboard", (req, res) => {
+  const items = ["a", "b", "c", "d", "e"];
+
+  res.render("dashboard", { items });
+});
+
+app.listen(3000, () => console.log("Running"));
